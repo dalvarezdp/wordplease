118c71f17df70375eec0be8ac0ae73654f6666c8
David Álvarez <david11988@gmail.com>
2017-03-13 20:09:53 +0100

    Creamos el modelo y los añadimos al admin para poder gestionarlos

--- src/blogs/models.py
+++ src/blogs/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+
+    class Meta:
+        verbose_name_plural = "Posts"
+
+    title = models.CharField(max_length=100)
+    subtitle = models.CharField(max_length=200)
+    description = models.TextField(blank=False, null=False)
+    image = models.ImageField(upload_to='post', verbose_name='image')
+    categories = models.ManyToManyField(Category)
+    date_public = models.DateTimeField(db_index=False, auto_now_add=False)
+    created_at = models.DateTimeField(auto_now_add=True)  # Añade automáticamente la fecha de creación
+    modified_at = models.DateTimeField(auto_now=True)  # Actualiza la fecha al guardar automáticamente
+    owner = models.ForeignKey(User, related_name="owned_posts")
+
+    def __str__(self):
+        return self.name
+
